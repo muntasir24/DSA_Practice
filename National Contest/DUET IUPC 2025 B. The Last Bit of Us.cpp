0bdc0e7892fdd466b49035f66645d8708082edbc
muntasir24 <azizmuntasir@gmail.com>
2026-07-03 17:45:59 +0600

    solved tree prob

--- National Contest/DUET IUPC 2025 B. The Last Bit of Us.cpp
+++ National Contest/DUET IUPC 2025 B. The Last Bit of Us.cpp
@@ -1,12 +1,57 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int dfs(int node,int parent, vector<vector<int>> &adj, vector<int> &v, int &ans)
+{
+
+    for (auto child : adj[node])
+    {
+        if(child!=parent){
+            v[node] ^= dfs(child, node, adj, v, ans);
+            // cout << " loop " << node << " " << v[node] << " ans : " << ans << endl;
+        }
+    }
+
+    if (v[node])
+        ans++;
+
+    // cout <<" outside loop "<< node << " " << v[node]<<" ans : "<<ans << endl;
+    return v[node];
+}
+
+int main()
+{
     int t;
     cin >> t;
-    while(t--){
-        
-        
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> v(n + 1);
+        int cnt = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            cin >> v[i];
+            if(v[i])
+                cnt++;
+        }
+       
+        vector<vector<int>> adj(n + 1);
+        for (int i = 0; i < n - 1; i++)
+        {
+            int x, y;
+            cin >> x >> y;
+            adj[x].push_back(y);
+            adj[y].push_back(x);
+        }
+        if (cnt % 2)
+        {
+            cout << -1 << endl;
+            continue;
+        }
+        int ans = 0;
+        dfs(1,1, adj, v, ans);
+        cout << ans << endl;
     }
 
     return 0;
